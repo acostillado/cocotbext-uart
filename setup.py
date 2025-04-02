fcb656a6a94f9e880a005c9d53fadacf5c5aa338
Daniel J. Mazure <daniel.mazure.external@sonova.com>
2025-04-02 12:39:46 +0000

    Fix installing

--- setup.py
+++ setup.py
@@ -1,2 +1,14 @@
-from setuptools import setup
-setup()
+from setuptools import setup, find_packages
+
+# Read version from file without importing the module
+version_ns = {}
+with open("cocotbext/uart/version.py") as f:
+    exec(f.read(), version_ns)
+
+setup(
+    name="cocotbext-uart",
+    version=version_ns["__version__"],
+    packages=find_packages(),
+    install_requires=["cocotb>=1.4"],
+)
+
